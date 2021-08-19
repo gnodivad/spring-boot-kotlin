499e52254e0970446db87422a49768fd9e3a4a83
David Ong <dodwei.dev@gmail.com>
2021-08-19 11:23:10 +0800

    [LearningKotlin]Creating a string with multiple lines

--- LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringVariables.kt
+++ LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringVariables.kt
@@ -11,4 +11,12 @@ fun main(args: Array<String>) {
 
     println("Hello $name ${surname.uppercase(Locale.getDefault())}")
     println("Your first \$name has ${name.length} characters")
+
+    val story =  """It was a dark and stormy night
+        |A foul smell crept across the city.
+        |Jane wondered what time it was, and when it would by daylight""".trimMargin("|")
+    println(story)
+
+    val changedStory = story.replaceAfterLast("it", " would be dawn.")
+    println(changedStory)
 }
