afaab37aa8616a0c8fbae95214d380539ca0cc7a
David Ong <dodwei.dev@gmail.com>
2021-08-19 11:44:25 +0800

    [LearningKotlin]Casting from one data type to another Java version

--- LearningKotlin/src/com/gnodivad/learningkotlin/java/BoringJavaCode.java
+++ LearningKotlin/src/com/gnodivad/learningkotlin/java/BoringJavaCode.java
@@ -1,7 +1,30 @@
 package com.gnodivad.learningkotlin.java;
 
+import java.math.BigDecimal;
+import java.util.Random;
+
 public class BoringJavaCode {
     public static void main(String[] args) {
-        String name = "David";
+//        String name = "David";
+
+        Object result;
+
+        Integer randomNumber = new Random().nextInt(3);
+        if (randomNumber == 1) {
+            result = new BigDecimal(30);
+        } else {
+            result = "hello";
+        }
+
+        System.out.println("Result is currently " + result);
+
+        if (result instanceof BigDecimal) {
+            result = ((BigDecimal)result).add(new BigDecimal(47));
+        } else {
+            String tempResult = (String)result;
+            result = tempResult.toUpperCase();
+        }
+
+        System.out.println("Result is currently " + result);
     }
 }
