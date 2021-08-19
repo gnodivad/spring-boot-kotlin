205537bd886a69a85ffd9b07e970b3529acfc7aa
David Ong <dodwei.dev@gmail.com>
2021-08-19 11:51:30 +0800

    [LearningKotlin]Casting from one data type to another Kotlin version

---
+++ LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringCasting.kt
@@ -0,0 +1,26 @@
+package com.gnodivad.learningkotlin.kotlin
+
+import java.math.BigDecimal
+import java.util.*
+
+fun main() {
+    var result: Any;
+
+    val randomNumber = Random().nextInt(3)
+    if (randomNumber == 1) {
+        result = BigDecimal(30)
+    } else {
+        result = "hello"
+    }
+
+    println("Result is currently $result")
+
+    if (result is BigDecimal) {
+        result = result.add(BigDecimal(47))
+    } else {
+        val tempResult = result as String
+        result = tempResult.toUpperCase()
+    }
+
+    println("Result is currently $result");
+}
