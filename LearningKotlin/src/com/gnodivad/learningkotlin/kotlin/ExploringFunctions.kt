bdc7ef128b79774e9807aa5b33bece6ab045a567
David Ong <dodwei.dev@gmail.com>
2021-08-20 10:33:05 +0800

    [LearningKotlin]Single expression functions

--- LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringFunctions.kt
+++ LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringFunctions.kt
@@ -4,6 +4,10 @@ fun printAString(value: String) {
     println(value)
 }
 
+fun addTwoNumbers(one: Double, two: Double) = one + two
+
 fun main() {
     printAString("Hello world")
+
+    println(addTwoNumbers(17.3, 9.6))
 }
