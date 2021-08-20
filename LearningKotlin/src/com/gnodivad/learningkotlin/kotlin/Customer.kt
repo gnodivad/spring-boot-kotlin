00ee0bcaff4d01a117dc201a60d89fb8c4024ff7
David Ong <dodwei.dev@gmail.com>
2021-08-20 11:44:31 +0800

    [LearningKotlin]Creating a simple constructor

--- LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/Customer.kt
+++ LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/Customer.kt
@@ -1,12 +1,11 @@
 package com.gnodivad.learningkotlin.kotlin
 
-class Customer {
-    val name: String = "David"
-    val address: String = "Singapore"
-    val age: Int = 21
+class Customer(val name: String, val address: String, var age: Int)  {
+
 }
 
 fun main() {
-    val customer = Customer()
+    val customer = Customer("Ong", "Malaysia", 22)
+    customer.age = 23
     println("${customer.name} is ${customer.age} years old")
 }
