d8bc467150385d1769e6fd9488c121d979950a77
David Ong <dodwei.dev@gmail.com>
2021-08-20 14:55:44 +0800

    [LearningKotlin]Alternative class design

--- LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/Customer.kt
+++ LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/Customer.kt
@@ -1,19 +1,27 @@
 package com.gnodivad.learningkotlin.kotlin
 
 class Customer(val name: String, val address: String, var age: Int)  {
-    constructor(name: String, age: Int): this(name, "", age) {
-        println("secondary constructor")
-    }
+    constructor(name: String, age: Int): this(name, "", age)
+}
+
+class AlternativeCustomer(val name: String, val age: Int) {
+    var address: String
 
     init {
-        println("init block")
+        address = ""
+    }
+
+    constructor(name: String, address: String, age: Int): this(name, age) {
+        this.address = address
     }
 }
 
+class AnotherAlternativeCustomer(val name: String, var age: Int, val address: String = "")
+
 fun main() {
-    val customer = Customer("Ong", "Malaysia", 22)
+    val customer = AnotherAlternativeCustomer("Ong", address = "Malaysia", age = 22)
     customer.age = 23
-    val customer2 = Customer("John", 31)
+    val customer2 = AnotherAlternativeCustomer("John", 31)
     println("${customer.name} is ${customer.age} years old")
     println("${customer2.name} is ${customer2.age} years old")
 }
