38bc5d2a63d0d6488eef398f56cd426c1cce3033
David Ong <dodwei.dev@gmail.com>
2021-08-20 15:04:54 +0800

    [LearningKotlin]Functions within a class

--- LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/Customer.kt
+++ LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/Customer.kt
@@ -29,6 +29,9 @@ class AnotherAlternativeCustomer(val name: String, var age: Int, val address: St
 
     val nextAge
     get() = age + 1
+
+    fun uppercaseName() = name.toUpperCase()
+    
 }
 
 fun main() {
@@ -39,5 +42,5 @@ fun main() {
     customer2.approved = true
     println("${customer.name} is ${customer.age} years old and is ${customer.approved}")
     println("${customer2.name} is ${customer2.age} years old and is ${customer2.approved}")
-    println("next year matt will be ${customer.nextAge}")
+    println("next year ${customer.uppercaseName()} will be ${customer.nextAge}")
 }
