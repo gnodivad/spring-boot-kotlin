a8b7125692458aac3858cea79f492b405f8d4292
David Ong <dodwei.dev@gmail.com>
2021-08-20 10:37:26 +0800

    [LearningKotlin]Using optional parameters in functions

--- LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringFunctions.kt
+++ LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringFunctions.kt
@@ -4,7 +4,7 @@ fun printAString(value: String) {
     println(value)
 }
 
-fun addTwoNumbers(one: Double, two: Double) = one + two
+fun addTwoNumbers(one: Double = 6.2, two: Double = 3.9) = one + two
 
 fun printSomeMaths(one: Double, two: Double) {
     println("one + two is ${one + two}")
@@ -15,5 +15,7 @@ fun main() {
     printAString("Hello world")
 
     println(addTwoNumbers(17.3, 9.6))
+    println(addTwoNumbers(5.6))
+    println(addTwoNumbers(two = 5.6))
     printSomeMaths(two = 17.3, one = 9.6)
 }
