b7c4c2e07cf16ef211100a8a38a04b48ed9ae626
David Ong <dodwei.dev@gmail.com>
2021-08-20 10:35:09 +0800

    [LearningKotlin]Using named parameters when calling functions

--- LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringFunctions.kt
+++ LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringFunctions.kt
@@ -6,8 +6,14 @@ fun printAString(value: String) {
 
 fun addTwoNumbers(one: Double, two: Double) = one + two
 
+fun printSomeMaths(one: Double, two: Double) {
+    println("one + two is ${one + two}")
+    println("one - two is ${one - two}")
+}
+
 fun main() {
     printAString("Hello world")
 
     println(addTwoNumbers(17.3, 9.6))
+    printSomeMaths(two = 17.3, one = 9.6)
 }
