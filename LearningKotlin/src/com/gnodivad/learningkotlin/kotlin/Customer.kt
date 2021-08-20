52ee2c9fdf5be574263f4f488f72b3f6f136883b
David Ong <dodwei.dev@gmail.com>
2021-08-20 14:51:37 +0800

    [LearningKotlin]Adding secondary constructors to a class

--- LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/Customer.kt
+++ LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/Customer.kt
@@ -1,11 +1,19 @@
 package com.gnodivad.learningkotlin.kotlin
 
 class Customer(val name: String, val address: String, var age: Int)  {
+    constructor(name: String, age: Int): this(name, "", age) {
+        println("secondary constructor")
+    }
 
+    init {
+        println("init block")
+    }
 }
 
 fun main() {
     val customer = Customer("Ong", "Malaysia", 22)
     customer.age = 23
+    val customer2 = Customer("John", 31)
     println("${customer.name} is ${customer.age} years old")
+    println("${customer2.name} is ${customer2.age} years old")
 }
