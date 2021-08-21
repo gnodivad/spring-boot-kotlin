66e67da1b080040d3d7301bda6a6fc679ac6e1ed
David Ong <dodwei.dev@gmail.com>
2021-08-21 10:55:40 +0800

    [Exercise1]Assigning a variable using an if expression

--- Exercise1/src/KotlinPerson.kt
+++ Exercise1/src/KotlinPerson.kt
@@ -15,12 +15,7 @@ data class KotlinPerson(val id: Long, val title: String, val firstName: String, 
             val today = GregorianCalendar()
             val years = today.get(Calendar.YEAR) - dateOfBirth.get(Calendar.YEAR)
 
-            return if (dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.YEAR)) {
-                years -1
-            }
-            else {
-                years
-            }
+            return if (dateOfBirth.get(Calendar.DAY_OF_YEAR) >= today.get(Calendar.YEAR)) years -1 else years
         }
     }
 }
@@ -31,4 +26,7 @@ fun main() {
     println("$john's age is ${john.age}");
     println("$jane's age is ${jane.age}");
     println("The age of someone born on 3rd May 1988 is ${KotlinPerson.getAge(GregorianCalendar(1988,5,3))}");
+
+    val olderPerson = if (john.age > jane.age) john else jane
+    println("The older person was $olderPerson")
 }
