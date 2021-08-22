bab0cde85f863e308cc33716df30e7dc0dcbc581
David Ong <dodwei.dev@gmail.com>
2021-08-22 13:27:36 +0800

    [Exercise1]Creating a mutable list

--- Exercise1/src/ExploringCollections.kt
+++ Exercise1/src/ExploringCollections.kt
@@ -2,4 +2,8 @@ fun main() {
     // immutable list
     val colors = listOf("Red", "Green", "Blue")
     println(colors::class.qualifiedName)
+
+    // mutable list
+    val days = mutableListOf("Monday", "Tuesday", "Wednesday")
+    val numbers = mutableListOf<Int>()
 }
