e0a5657863fb1f104313767084a8e4247f9a9eef
David Ong <dodwei.dev@gmail.com>
2021-08-22 12:51:42 +0800

    [Exercise1]Using for to loop through a collection

--- Exercise1/src/ExploringLoops.kt
+++ Exercise1/src/ExploringLoops.kt
@@ -1,8 +1,19 @@
 fun main() {
+    // while Loops
     var x: Int = 0
 
     while (x < 10) {
         println(x)
         x++
+    }
+
+    val people = ArrayList<KotlinPerson>()
+    people.add(KotlinPerson(1, "Mr", "James", "Apple", null))
+    people.add(KotlinPerson(1, "Miss", "Sophie", "Orange", null))
+    people.add(KotlinPerson(1, "Mrs", "Anita", "Kumkwat", null))
+    people.add(KotlinPerson(1, "Mr", "Darren", "Banana", null))
+
+    for (person in people) {
+        println(person)
     }
 }
