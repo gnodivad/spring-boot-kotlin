42a302058947e632355a8a378e5d69d361f37b0c
David Ong <dodwei.dev@gmail.com>
2021-08-22 13:31:40 +0800

    [Exercise1]Working with Arrays in Kotlin

--- Exercise1/src/ExploringCollections.kt
+++ Exercise1/src/ExploringCollections.kt
@@ -9,7 +9,16 @@ fun main() {
 
     // sets
     val months = mutableSetOf("Jan", "Feb")
-    
+
     // maps
     val webColors = mapOf("red" to "ff0000", "blue" to "00ff00")
+
+    // arrays
+    val intArray: IntArray = intArrayOf(1, 2, 3, 4, 5)
+    intArray.set(3, -4)
+    intArray[4] = 9
+    intArray.forEach { print("$it ") }
+
+    println()
+    println(colors[2])
 }
