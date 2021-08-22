67ff53f23477b147fcd5d966e31e1d9bc0511be4
David Ong <dodwei.dev@gmail.com>
2021-08-22 22:22:51 +0800

    [LearningKotlin]Testing for exceptions

--- LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringTesting.kt
+++ LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringTesting.kt
@@ -1,5 +1,6 @@
 package com.gnodivad.learningkotlin.kotlin
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.time.Year
 import java.util.*
@@ -9,6 +10,7 @@ import kotlin.test.fail
 class AgeCalculation() {
     fun getAge(dob: Calendar): Int {
         val today = Calendar.getInstance()
+        if (dob.timeInMillis > today.timeInMillis) throw IllegalArgumentException()
         val years = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR)
         return if (dob.get(Calendar.DAY_OF_YEAR) > today.get(Calendar.DAY_OF_YEAR))
             years - 1
@@ -18,10 +20,11 @@ class AgeCalculation() {
 }
 
 class AgeCaculationTests() {
+    val ageCalcClass = AgeCalculation()
 
     @Test
     fun checkAgeWhenBornToday() {
-        assertEquals(0, AgeCalculation().getAge(Calendar.getInstance()))
+        assertEquals(0, ageCalcClass.getAge(Calendar.getInstance()))
     }
 
     @Test
@@ -29,6 +32,16 @@ class AgeCaculationTests() {
         val date = Calendar.getInstance()
         date.add(Calendar.DAY_OF_YEAR, -1000)
 
-        assertEquals(2, AgeCalculation().getAge(date))
+        assertEquals(2, ageCalcClass.getAge(date))
+    }
+
+    @Test
+    fun testForException() {
+        val date = Calendar.getInstance()
+        date.add(Calendar.DAY_OF_YEAR, 10)
+
+        Assertions.assertThrows(IllegalArgumentException::class.java) {
+            ageCalcClass.getAge(date)
+        }
     }
 }
