9c8ed2705a3ffaadca3beffcd367aed12146b2cd
David Ong <dodwei.dev@gmail.com>
2021-08-22 22:04:44 +0800

    [LearningKotlin]Try as an expression

--- LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringExceptions.kt
+++ LearningKotlin/src/com/gnodivad/learningkotlin/kotlin/ExploringExceptions.kt
@@ -2,9 +2,16 @@ package com.gnodivad.learningkotlin.kotlin
 
 @Throws(InterruptedException::class)
 fun divide(a: Int, b: Int): Double {
+    Thread.sleep(1000)
     return (a.toDouble()) / b
 }
 
 fun main() {
-    divide(5, 23)
+    var result = try {
+        divide(5, 23)
+    } catch (e: Exception) {
+        0
+    }
+
+    println(result)
 }
