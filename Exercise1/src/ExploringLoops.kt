9a3390ef4b3a65acd10a51794729075995443967
David Ong <dodwei.dev@gmail.com>
2021-08-22 13:15:29 +0800

    [Exercise1]Destructuring with a for loop

--- Exercise1/src/ExploringLoops.kt
+++ Exercise1/src/ExploringLoops.kt
@@ -7,13 +7,13 @@ fun main() {
         x++
     }
 
-    val people = ArrayList<KotlinPerson>()
-    people.add(KotlinPerson(1, "Mr", "James", "Apple", null))
-    people.add(KotlinPerson(1, "Miss", "Sophie", "Orange", null))
-    people.add(KotlinPerson(1, "Mrs", "Anita", "Kumkwat", null))
-    people.add(KotlinPerson(1, "Mr", "Darren", "Banana", null))
+    val people = HashMap<Int, KotlinPerson>()
+    people.put(1, KotlinPerson(1, "Mr", "James", "Apple", null))
+    people.put(2, KotlinPerson(2, "Miss", "Sophie", "Orange", null))
+    people.put(3, KotlinPerson(3, "Mrs", "Anita", "Kumkwat", null))
+    people.put(4, KotlinPerson(4, "Mr", "Darren", "Banana", null))
 
-    for (person in people) {
-        println(person)
+    for ((key, value) in people) {
+        println("$value has key $key")
     }
 }
