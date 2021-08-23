5e326101b4b27d646a443e6e1fe46ba326458b88
David Ong <dodwei.dev@gmail.com>
2021-08-23 12:10:04 +0800

    [kotlinJavaInteroperability]Importing a Kotlin jar into a Java project

---
+++ JavaKotlinInteroperability2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        CustomerDatabase db = new CustomerDatabase();
+        for (Customer c : db.getCustomers()) {
+            System.out.println(c);
+        }
+    }
+}
