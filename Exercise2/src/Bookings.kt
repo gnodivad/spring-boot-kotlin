e3c586b2330826d73baa1a42c794adccf0aea87c
David Ong <dodwei.dev@gmail.com>
2021-08-23 10:53:47 +0800

    [Exercise2]Class inheritance

--- Exercise2/src/Bookings.kt
+++ Exercise2/src/Bookings.kt
@@ -7,10 +7,16 @@ interface BookingManager {
     fun systemStatus() = "All Operations are Functional"
 }
 
-class BasicBookingManager : BookingManager {
+open class BasicBookingManager : BookingManager {
     override val version = "1.0"
 
     override fun isSeatFree(seat: Seat) = true
 
     override fun reserveSeat(seat: Seat, customerId: Long) = false
+}
+
+class AdvancedBookingManager: BasicBookingManager() {
+    override val version = "2.0"
+
+    fun howManyBookings() = 10
 }
