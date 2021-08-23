00cc672f62638bffe5f528479bdadafe807dd0cf
David Ong <dodwei.dev@gmail.com>
2021-08-23 11:57:08 +0800

    [kotlinJavaInteroperability]Using immutable lists

--- KotlinJavaInteroperability/src/Main.java
+++ KotlinJavaInteroperability/src/Main.java
@@ -1,5 +1,10 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Customer phil = new Customer(0, "Phil");
+        CustomerDatabase db = new CustomerDatabase();
+        List<Customer> customers = db.getCustomers();
+        customers.add(phil);
     }
 }
