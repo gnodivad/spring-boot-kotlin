c6d5559c49a58620d94e4593a6cb0b9d1eed73e1
David Ong <dodwei.dev@gmail.com>
2021-08-25 11:58:24 +0800

    [theater]Mappings between entities

---
+++ theater/src/main/kotlin/com/gnodivad/theater/domain/Booking.kt
@@ -0,0 +1,15 @@
+package com.gnodivad.theater.domain
+
+import javax.persistence.*
+
+@Entity
+data class Booking(
+    @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long,
+    val customerName: String
+) {
+    @ManyToOne
+    lateinit var seat: Seat
+
+    @ManyToOne
+    lateinit var performance: Performance
+}
