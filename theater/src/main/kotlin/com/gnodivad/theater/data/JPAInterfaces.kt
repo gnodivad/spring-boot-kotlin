3b8b65c255917c5a2eab17ee4b8f98fa2eb33951
David Ong <dodwei.dev@gmail.com>
2021-08-25 11:49:05 +0800

    [theater]adding in the performances functionality

--- theater/src/main/kotlin/com/gnodivad/theater/data/JPAInterfaces.kt
+++ theater/src/main/kotlin/com/gnodivad/theater/data/JPAInterfaces.kt
@@ -1,6 +1,9 @@
 package com.gnodivad.theater.data
 
+import com.gnodivad.theater.domain.Performance
 import com.gnodivad.theater.domain.Seat
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface SeatRepository : JpaRepository<Seat, Long>
+
+interface PerformanceRepository : JpaRepository<Performance, Long>
